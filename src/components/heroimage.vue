d06967f3c3e649beca0bd973fc8877f39c4990ed
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:06 +0530

    Create heroimage.vue

---
+++ src/components/heroimage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <img class="img-fluid w-100" id="imgheifht" src="../assets/img/pets-care-sd.webp">
+    </div> 
+</template>
+          
+<script>
+     export default {
+            name: 'heroimage'     
+    }
+</script>
+
+<style scoped>
+@media (max-width:800px){
+.w-100 {
+    width: 100% !important;
+    margin-top: 55px;
+}}
+
+@media (min-width:1200px){
+#imgheifht {
+    height: 602px;
+    margin-top: 55px;
+}}
+
+.herotittle{
+    text-align: center;
+}
+</style>
+          
+        
