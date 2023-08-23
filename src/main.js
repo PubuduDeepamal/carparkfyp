967e9b65e2f4d74e03108ba98037c342d0a10b04
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:46 +0530

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+
+import './assets/main.css'
+
+
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
+import 'bootstrap/dist/js/bootstrap.js'
