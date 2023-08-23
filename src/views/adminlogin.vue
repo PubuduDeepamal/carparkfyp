a2ee4c4e7e6f972fa20929fc45caa23d4b460811
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:57 +0530

    Create adminlogin.vue

---
+++ src/views/adminlogin.vue
@@ -0,0 +1,13 @@
+<script setup>
+import heder4 from '@/components/heder4.vue'
+import adminloginhome from '@/components/adminloginhome.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder4/>
+    <adminloginhome/>
+    <footersection/>
+  </main>
+</template>
