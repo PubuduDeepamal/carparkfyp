0132ba25dd59f2c11a5a19f376c6cbf5014db875
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:04 +0530

    Create HelloWorld.vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
