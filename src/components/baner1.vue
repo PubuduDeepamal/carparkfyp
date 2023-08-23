71f26b732c6b5626408e8c74b3418c17fb4e8822
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:19:43 +0530

    Create baner1.vue

---
+++ src/components/baner1.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img class="img-fluid w-100" src="../assets/img/banner-1.jpg">
+    </div>  
+</template>
+          
+<script>
+    export default {
+        name: 'baner1'   
+    }
+</script>
+
+<style scoped>
+.img-fluid {
+    margin-top: 63px;
+}
+</style>
