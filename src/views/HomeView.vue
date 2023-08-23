35f354a227e3cfb43765d1301162ccf0a5388a48
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:21:04 +0530

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import heder1 from '@/components/heder1.vue'
+import Services from '@/components/Services.vue'
+import baner1 from '@/components/baner1.vue'
+import health from '@/components/health.vue'
+import HomeProduct from '@/components/HomeProduct.vue'
+import footersection from '@/components/footersection.vue'
+import Donate from '@/components/Donate.vue'
+
+</script>
+
+<template>
+  <main>
+    <heder1/>
+    <baner1/>
+    <Services/>
+    <health/>
+    <HomeProduct/>
+    <Donate/>
+    <footersection/>
+  </main>
+</template>
