c7000ef9a398edafd7dc95a6d86b5bc8803c0e34
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:48 +0530

    Create AboutView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import heder1 from '@/components/heder1.vue'
+	import aboutus from '@/components/aboutus.vue'
+  import EmployeeDetails from '@/components/employee.vue'
+  import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder1/>
+    <aboutus/>
+    <EmployeeDetails/>
+    <footersection/>
+  </main>
+</template>
