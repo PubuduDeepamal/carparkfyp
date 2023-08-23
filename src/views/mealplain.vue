655db7d47b99a5dd0481b1a122ceb2886548b39b
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:21:08 +0530

    Create mealplain.vue

---
+++ src/views/mealplain.vue
@@ -0,0 +1,17 @@
+<script setup>
+import heder2 from '@/components/heder2.vue'
+import meal from '@/components/meal.vue'
+import meal1 from '@/components/meal1.vue'
+import mealview from '@/components/mealview.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder2/>
+    <meal/>
+    <meal1/>
+    <mealview/>
+    <footersection/>
+  </main>
+</template>
