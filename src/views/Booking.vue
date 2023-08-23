bbd9261d8695f4ef57a7739c67e274441d1f2ea9
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:43:08 +0530

    Create Booking.vue

---
+++ src/views/Booking.vue
@@ -0,0 +1,15 @@
+<script setup>
+import heder2 from '@/components/heder2.vue'
+import BookingSlot from '@/components/BookingSlot.vue'
+import mealview from '@/components/mealview.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder2/>
+    <BookingSlot/>
+    <mealview/>
+    <footersection/>
+  </main>
+</template>
