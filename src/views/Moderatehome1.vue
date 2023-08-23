f6f0703fa98f569d4778e0b14e387798b44b0a52
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:21:10 +0530

    Create Moderatehome1.vue

---
+++ src/views/Moderatehome1.vue
@@ -0,0 +1,13 @@
+<script setup>
+import heder5 from '@/components/heder5.vue'
+import adminhome13 from '@/components/adminhome13.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder5/>
+    <adminhome13/>
+    <footersection/>
+  </main>
+</template>
