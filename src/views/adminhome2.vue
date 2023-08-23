25bd66834c9ae2c68be8b33d9f468f6dbad7115f
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:21:00 +0530

    Create adminhome2.vue

---
+++ src/views/adminhome2.vue
@@ -0,0 +1,13 @@
+<script setup>
+import heder3 from '@/components/heder3.vue'
+import bookingupdate from '@/components/bookingupdate.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder3/>
+    <bookingupdate/>
+    <footersection/>
+  </main>
+</template>
