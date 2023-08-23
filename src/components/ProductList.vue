431cf92306d785605c3b7827d60164faa94ed4b1
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:31 +0530

    Create ProductList.vue

---
+++ src/components/ProductList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          {{ product.name }} - {{ product.quantity }}
+          <button @click="incrementQuantity(product)">+</button>
+          <button @click="decrementQuantity(product)">-</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        products: [
+          { id: 1, name: 'Product 1', quantity: 0 },
+          { id: 2, name: 'Product 2', quantity: 0 },
+          // Add more products as needed
+        ],
+      };
+    },
+    methods: {
+      incrementQuantity(product) {
+        product.quantity++;
+      },
+      decrementQuantity(product) {
+        if (product.quantity > 0) {
+          product.quantity--;
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add custom styles as needed */
+  </style>
+  
