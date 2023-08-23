1ca62fe0111aff371544aadcf1b27e7c0e805648
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:52 +0530

    Create adminhome1.vue

---
+++ src/views/adminhome1.vue
@@ -0,0 +1,17 @@
+<script setup>
+import heder3 from '@/components/heder3.vue'
+import admincrm from '@/components/admincrm.vue'
+import admincrmdisplay from '@/components/admincrmdisplay.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder3/>
+    <admincrm/>
+    <admincrmdisplay/>
+    <footersection/>
+  </main>
+</template>
+
+
