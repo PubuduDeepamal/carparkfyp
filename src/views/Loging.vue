660d290e2b1a9f42c93b7cb66aedfcf32bc716a1
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:21:05 +0530

    Create Loging.vue

---
+++ src/views/Loging.vue
@@ -0,0 +1,13 @@
+<script setup>
+import heder1 from '@/components/heder1.vue'
+import loging from '@/components/loging.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder1/>
+    <loging/>
+    <footersection/>
+  </main>
+</template>
