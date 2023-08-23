d4cbd10e5c2f469bf51d0827ecb4703acb1b2605
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:25:26 +0530

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBANo6uLaoGsboc9ex5R2HAOh1xHP3r9RI",
-  authDomain: "angelhaven-a68a5.firebaseapp.com",
-  databaseURL: "https://angelhaven-a68a5-default-rtdb.firebaseio.com",
-  projectId: "angelhaven-a68a5",
-  storageBucket: "angelhaven-a68a5.appspot.com",
-  messagingSenderId: "129958052805",
-  appId: "1:129958052805:web:a225b7f40d21fedb30c1ac"
+  apiKey: "AIzaSyDWfil-cgu_bPr3BkXW0wzJKDecF6VbaTI",
+  authDomain: "smart-parking-system-acf8a.firebaseapp.com",
+  databaseURL: "https://smart-parking-system-acf8a-default-rtdb.firebaseio.com",
+  projectId: "smart-parking-system-acf8a",
+  storageBucket: "smart-parking-system-acf8a.appspot.com",
+  messagingSenderId: "1012120949318",
+  appId: "1:1012120949318:web:08edd146ca80508aacf047",
+  measurementId: "G-FFPDVV50TN"
 };
 
 const firbaseApp = initializeApp(firebaseConfig);
