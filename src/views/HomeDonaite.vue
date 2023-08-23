6a67afd6fa8f9fcb5419c354ca4321486ef2246a
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:21:02 +0530

    Create HomeDonaite.vue

---
+++ src/views/HomeDonaite.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import heder1 from '@/components/heder1.vue'
+	import meal from '@/components/meal.vue'
+  import Donate from '@/components/Donate.vue'
+  import contact from '@/components/contact.vue'
+  import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder1/>
+    <meal/>
+    <Donate/>
+    <contact/>
+    <footersection/>
+  </main>
+</template>
