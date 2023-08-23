22030e006b55629642fbfea47579615d442f9efd
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:39:48 +0530

    Update HomeDonaite.vue

--- src/views/HomeDonaite.vue
+++ src/views/HomeDonaite.vue
@@ -1,6 +1,5 @@
 <script setup>
   import heder1 from '@/components/heder1.vue'
-	import meal from '@/components/meal.vue'
   import Donate from '@/components/Donate.vue'
   import contact from '@/components/contact.vue'
   import footersection from '@/components/footersection.vue'
@@ -9,7 +8,6 @@
 <template>
   <main>
     <heder1/>
-    <meal/>
     <Donate/>
     <contact/>
     <footersection/>
