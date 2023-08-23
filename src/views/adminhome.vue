f7101ba38df4ba8c75d680f885097989ac6361a2
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 04:20:55 +0530

    Create adminhome.vue

---
+++ src/views/adminhome.vue
@@ -0,0 +1,15 @@
+<script setup>
+import heder3 from '@/components/heder3.vue'
+import adminhome11 from '@/components/adminhome11.vue'
+import adminhome12 from '@/components/adminhome12.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder3/>
+    <adminhome11/>
+    <adminhome12/>
+    <footersection/>
+  </main>
+</template>
