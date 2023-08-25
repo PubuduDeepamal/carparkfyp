435f47eb01be9eaeb8fa2ba38b4ee4e858afbbc7
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-25 21:28:17 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,9 +10,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/Slots',
+      name: 'Slots',
+      component: () => import('../views/Slots.vue')
     },
     {
       path: '/Contactus',
