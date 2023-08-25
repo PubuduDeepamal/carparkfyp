a2b42669b1ea84877a75fed175d25bc74938c665
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-25 21:29:01 +0530

    Create Slots.vue

---
+++ src/views/Slots.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+  import heder1 from '@/components/heder1.vue'
+  import AvailableSlotsHero from '@/components/Hero/AvailableSlots.vue'
+  import slotdetails from '@/components/slotdetails.vue'
+  import footersection from '@/components/footersection.vue'
+
+</script>
+
+<template>
+  <main>
+    <heder1/>
+    <AvailableSlotsHero/>
+    <slotdetails/>
+    <footersection/>
+  </main>
+</template>
