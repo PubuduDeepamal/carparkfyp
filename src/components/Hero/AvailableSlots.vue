ef2ac7e7e52e270391789aa2a9aa63c3e86cad83
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-25 21:07:23 +0530

    Create AvailableSlots.vue

---
+++ src/components/Hero/AvailableSlots.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <div class="image-container">
+        <img class="img-fluid w-100" style="height: 500px;" src="../../assets/img/carpark.jpeg">
+        <div class="image-text">
+          <p>Available Slots</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'baner1'
+  }
+  </script>
+  
+  <style scoped>
+  .img-fluid {
+    margin-top: 63px;
+  }
+  
+  .image-container {
+    position: relative;
+  }
+  
+  .image-text {
+    position: absolute;
+    top: 300px;
+    left: 39%; 
+    color: white; 
+    font-size: 65px; 
+    font-weight: bold;
+    font-family: 'New Time Nevran', sans-serif;
+  }
+  </style>
+  
