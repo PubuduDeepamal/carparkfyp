ff802a3e10c95be9be94d97f444e92e65bcdaef1
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-25 21:09:26 +0530

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,15 @@
 <script setup>
+
   import heder1 from '@/components/heder1.vue'
-	import aboutus from '@/components/aboutus.vue'
   import AvailableSlots from '@/components/Hero/AvailableSlots.vue'
-  import EmployeeDetails from '@/components/employee.vue'
   import footersection from '@/components/footersection.vue'
+  
 </script>
 
 <template>
   <main>
     <heder1/>
     <AvailableSlots/>
-    <aboutus/>
-    <EmployeeDetails/>
     <footersection/>
   </main>
 </template>
