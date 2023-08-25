ec7404ebc6de66a92159cec720114935410e27c0
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-25 13:29:27 +0530

    Update ContactusHero.vue

--- src/components/Hero/ContactusHero.vue
+++ src/components/Hero/ContactusHero.vue
@@ -1,17 +1,36 @@
 <template>
     <div>
+      <div class="image-container">
         <img class="img-fluid w-100" style="height: 600px;" src="../../assets/img/carpark.jpeg">
-    </div>  
-</template>
-          
-<script>
-    export default {
-        name: 'baner1'   
-    }
-</script>
-
-<style scoped>
-.img-fluid {
+        <div class="image-text">
+          <p>Your Text Here</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'baner1'
+  }
+  </script>
+  
+  <style scoped>
+  .img-fluid {
     margin-top: 63px;
-}
-</style>
+  }
+  
+  .image-container {
+    position: relative;
+  }
+  
+  .image-text {
+    position: absolute;
+    top: 340px;
+    left: 38%; 
+    color: white; 
+    font-size: 56px; 
+    font-weight: bold;
+  }
+  </style>
+  
