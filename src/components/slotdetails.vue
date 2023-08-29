edf4bd6d3d6212878ebf691b40e9e080321ecd3f
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-29 17:07:29 +0530

    Update slotdetails.vue

--- src/components/slotdetails.vue
+++ src/components/slotdetails.vue
@@ -1,63 +1,67 @@
 <template>
-  <br><br><br>
-    <div class="container">
-        <div class="section-title">
-          <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 60px;">Booking Details</h3>
+  <div class="container">
+    <div class="section-title">
+      <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 60px;">Booking Details</h3>
     </div>
 
     <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <table class="table table-striped table-mobile-responsive table-mobile-sided">
-                        <thead>
-                            <tr>
-                              <th scope="col">Meal</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                          <tr v-for="item in items" :key="item.id">
-                            <td>{{ item.Gate_01 }}</td>
-                          </tr>
-                        </tbody>
-                    </table>
-                </div>
-              </div>
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <table class="table table-striped table-mobile-responsive table-mobile-sided">
+              <thead>
+                <tr>
+                  <th scope="col">Gate_01</th>
+                  <th scope="col">Gate_02</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in items" :key="item.id">
+                  <td>{{ item.Gate_01 }}</td>
+                  <td>{{ item.Gate_02 }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-<br>
-  </template>
-  
-    
-    
-  <script>
-  import { collection, getDocs } from 'firebase/firestore';
-  import db from '../firebase/init.js';
-  
-  export default {
-    data() {
-      return {
-        items: [],
-      };
-    },
-    mounted() {
-      this.fetchData();
-    },
-    methods: {
-      fetchData() {
-        getDocs(collection(db, 'Gate_Status'))
-          .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-              this.items.push(doc.data());
-            });
-            // Log items to check if data is retrieved correctly
-            console.log('Fetched Data:', this.items);
-          })
-          .catch(error => {
-            console.error('Error fetching data:', error);
-          });
-      },
-    },
-  };
-  </script>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'; // If you're using Vue 3
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+import db from '../firebase/init.js';
+
+export default {
+  setup() {
+    const items = ref([]);
+
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'Gate_Status'));
+        items.value = querySnapshot.docs.map((doc) => doc.data());
+        console.log('Fetched Data:', items.value);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    onMounted(() => {
+      fetchData(); // Fetch data initially
+
+      // Set up a real-time listener for changes in Firestore
+      const gateStatusCollection = collection(db, 'Gate_Status');
+      onSnapshot(gateStatusCollection, (querySnapshot) => {
+        items.value = querySnapshot.docs.map((doc) => doc.data());
+        console.log('Updated Data:', items.value);
+      });
+    });
+
+    return {
+      items,
+    };
+  },
+};
+</script>
