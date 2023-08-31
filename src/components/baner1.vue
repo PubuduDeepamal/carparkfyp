feecd668fd7fecea6e833e438032da18274470ce
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-31 10:18:11 +0530

    Update baner1.vue

--- src/components/baner1.vue
+++ src/components/baner1.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img class="img-fluid w-100" src="../assets/img/banner-1.jpg">
+        <img class="img-fluid w-100" src="https://img.freepik.com/free-photo/shiny-sports-car-racing-through-futuristic-cityscape-generated-by-ai_188544-29674.jpg?t=st=1693457117~exp=1693460717~hmac=7cf535cd811949cabf9972a2ea2542a9429d88422944182bf1a5c7a714b8d9db&w=1380" style="height: 50%;">
     </div>  
 </template>
           
