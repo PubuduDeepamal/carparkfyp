5748116e1e5bd2ebbc4e6e476110751a0338af5f
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-01 13:31:31 +0530

    Update adminhome13.vue

--- src/components/adminhome13.vue
+++ src/components/adminhome13.vue
@@ -1,72 +1,79 @@
 <template>
-    <br><br><br>
-      <div class="container">
-          <div class="section-title">
-            <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 60px;">Booking Details</h3>
-      </div>
-  
-      <div class="row">
-          <div class="col">
-              <div class="card">
-                  <div class="card-body">
-                      <table class="table table-striped table-mobile-responsive table-mobile-sided">
-                          <thead>
-                              <tr>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
-                                <th scope="col">Contact Number</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Date</th>
-                                <th scope="col">Time</th>
-                              </tr>
-                          </thead>
-                          <tbody>
-                            <tr v-for="item in items" :key="item.id">
-                              <td>{{ item.Meal }}</td>
-                              <td>{{ item.Position }}</td>
-                              <td>{{ item.contact }}</td>
-                              <td>{{ item.dateInput }}</td>
-                              <td>{{ item.firstName }}</td>
-                              <td>{{ item.lastName }}</td>
-                            </tr>
-                          </tbody>
-                      </table>
-                  </div>
-                </div>
-          </div>
-      </div>
-  </div>
   <br>
-    </template>
+    <div class="container">
+        <div class="section-title">
+          <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 40px;" id="fontfamily">Booking Details</h3>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-striped table-mobile-responsive table-mobile-sided">
+                        <thead>
+                            <tr>
+                              <th scope="col" id="fontfamily">First Name</th>
+                                <th scope="col" id="fontfamily">Last Name</th>
+                                <th scope="col" id="fontfamily">Contact Number</th>
+                                <th scope="col" id="fontfamily">Email</th>
+                                <th scope="col" id="fontfamily">Date</th>
+                                <th scope="col" id="fontfamily">Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="item in items" :key="item.id">
+                            <td id="fontfamily">{{ item.firstName }}</td>
+                            <td id="fontfamily">{{ item.lastName }}</td>
+                            <td id="fontfamily">{{ item.contact }}</td>
+                            <td id="fontfamily">{{ item.Email }}</td>
+                            <td id="fontfamily">{{ item.dateInput }}</td>
+                            <td id="fontfamily">{{ item.timeInput }}</td>
+                          </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+        </div>
+    </div>
+</div>
+<br>
+  </template>
+  
     
-      
-      
-      <script>
-      import { collection, getDocs } from 'firebase/firestore';
-      import db from '../firebase/init.js';
-      
-      export default {
-        data() {
-          return {
-            items: [],
-          };
-        },
-        mounted() {
-          this.fetchData();
-        },
-        methods: {
-          fetchData() {
-            getDocs(collection(db, 'booking'))
-              .then(querySnapshot => {
-                querySnapshot.forEach(doc => {
-                  this.items.push(doc.data());
-                });
-              })
-              .catch(error => {
-                console.error('Error fetching data:', error);
+    
+    <script>
+    import { collection, getDocs } from 'firebase/firestore';
+    import db from '../firebase/init.js';
+    
+    export default {
+      data() {
+        return {
+          items: [],
+        };
+      },
+      mounted() {
+        this.fetchData();
+      },
+      methods: {
+        fetchData() {
+          getDocs(collection(db, 'booking'))
+            .then(querySnapshot => {
+              querySnapshot.forEach(doc => {
+                this.items.push(doc.data());
               });
-          },
+            })
+            .catch(error => {
+              console.error('Error fetching data:', error);
+            });
         },
-      };
-      </script>
-      
+      },
+    };
+    </script>
+    
+  <style>
+
+  #fontfamily {
+      font-family: 'New Time Nevran', sans-serif;
+    }
+  
+  </style>
