c44ef39d5b549de04456b6dc9f83234aa21e301a
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-05 13:23:08 +0530

    Update adminhome2.vue

--- src/views/adminhome2.vue
+++ src/views/adminhome2.vue
@@ -1,13 +1,11 @@
 <script setup>
 import heder3 from '@/components/heder3.vue'
 import bookingupdate from '@/components/bookingupdate.vue'
-import footersection from '@/components/footersection.vue'
 </script>
 
 <template>
   <main>
     <heder3/>
     <bookingupdate/>
-    <footersection/>
   </main>
 </template>
