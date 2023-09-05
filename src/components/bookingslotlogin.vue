e8d7970ec4cd621bc74acc7cd6bc1be6e9e182be
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-05 13:10:51 +0530

    Update bookingslotlogin.vue

--- src/components/bookingslotlogin.vue
+++ src/components/bookingslotlogin.vue
@@ -16,7 +16,7 @@
             </div>
           
             <button id="btnsub" class="bg-blue-500 hover:bg-blue-600 absolute top-0 left-1/2 transform -translate-x-1/2 mt-2 py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-              Booking
+              <RouterLink to="/Loging">Booking</RouterLink>
             </button>
   
           </div>
