faa7820c4ca0758a110ca7e64cb21fbcf8d7f128
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-06 10:26:57 +0530

    Update slotdetails.vue

--- src/components/slotdetails.vue
+++ src/components/slotdetails.vue
@@ -11,6 +11,14 @@
     <div v-else>
       Loading data...
     </div>
+
+    <!-- Display Gate Status -->
+    <div>
+      <h2>Gate Status</h2>
+      <div v-for="(status, gate) in gateStatus" :key="gate">
+        <div>{{ gate }}: {{ status }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,10 +29,12 @@ export default {
   data() {
     return {
       postData: [],
+      gateStatus: {}, // To store gate status data
     };
   },
   mounted() {
     this.getPosts();
+    this.getGateStatus();
   },
   methods: {
     getPosts() {
@@ -43,9 +53,16 @@ export default {
         this.postData.push({ ...posts[key], id: key });
       }
     },
+    getGateStatus() {
+      axios
+        .get('https://smart-parking-system-acf8a-default-rtdb.firebaseio.com/Gate_Status.json')
+        .then((response) => {
+          this.gateStatus = response.data;
+        })
+        .catch((error) => {
+          console.error('Error fetching gate status:', error);
+        });
+    },
   },
 };
 </script>
-
-
-  
