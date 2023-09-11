5374a5e216ae035acf257749b69c9a7eb86b5ac5
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-11 18:09:01 +0530

    Create Adminhome10.vue

---
+++ src/views/Adminhome10.vue
@@ -0,0 +1,11 @@
+<script setup>
+import heder3 from '@/components/heder3.vue'
+import adminhome12 from '@/components/adminhome12.vue'
+</script>
+
+<template>
+  <main>
+    <heder3/>
+    <adminhome12/>
+  </main>
+</template>
