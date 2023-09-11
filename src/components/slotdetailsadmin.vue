f654c2c91e7b7e9db538579299bc951ad432a60a
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-11 15:21:25 +0530

    Update slotdetailsadmin.vue

--- src/components/slotdetailsadmin.vue
+++ src/components/slotdetailsadmin.vue
@@ -1,57 +1,38 @@
+
 <template>
-    <div>
-  
-      <!-- Display Parking Slots -->
-      <div class="my-4">
-        <h1 class="display-4 mb-4" id="GateStatus" style="margin-top: 50px;">Available Parking Slots</h1>
-        <div class="card">
-          <div class="card-body">
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th scope="col"><b>Slot Name</b></th>
-                  <th scope="col"><b>Slot Status</b></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(slotStatus, slotName) in parkingSlots" :key="slotName">
-                  <td>{{ slotName }}</td>
-                  <td>{{ slotStatus }}</td>
-                </tr>
-              </tbody>
-            </table>
+    <br><br><br>
+      <div class="container">
+          <div class="section-title">
+            <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 60px;">Slot Details</h3>
           </div>
-        </div>
-      </div>
-  
-      <!-- Display Gate Status -->
-      <div class="my-4">
-        <h1 class="display-4 mb-4" id="GateStatus">Gate Status</h1>
-        <div class="card">
-          <div class="card-body">
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th scope="col"><b>Gate</b></th>
-                  <th scope="col"><b>Status</b></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(status, gate) in gateStatus" :key="gate">
-                  <td>{{ gate }}</td>
-                  <td>{{ status }}</td>
-                </tr>
-              </tbody>
-            </table>
+        <div class="row">
+          <div class="col">
+              <div class="card">
+                  <div class="card-body">
+                      <table class="table table-striped table-mobile-responsive table-mobile-sided">
+                          <thead>
+                            <tr>
+                                <th scope="col"><b>Slot Name</b></th>
+                                <th scope="col"><b>Slot Status</b></th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr v-for="(slotStatus, slotName) in parkingSlots" :key="slotName">
+                            <td>{{ slotName }}</td>
+                            <td>{{ slotStatus }}</td>
+                            </tr>
+                          </tbody>
+                      </table>
+                  </div>
+                </div>
           </div>
-        </div>
       </div>
-  
     </div>
     <br>
   </template>
-  
-  <script>
+    
+    
+    <script>
   import axios from 'axios';
   
   export default {
@@ -93,44 +74,16 @@
     },
   };
   </script>
+    
+  <style>
+  .card {
+      margin-left: 200px
+    }
   
-  <style scoped>
   
-  #GateStatus {
-    font-size: 45.8px;
-    font-weight: bold;
-    text-align: center;
-    font-family: 'New Time Nevran', sans-serif;
-  }
-  
-  #GateStatus1 {
-    text-align: center;
-    font-family: 'New Time Nevran', sans-serif;
-    font-size: 25.8px;
-    text-decoration: none; /* Remove underlines */
-  }
-  
-  #GateStatus1:hover {
-    text-align: center;
-    font-family: 'New Time Nevran', sans-serif;
-    font-size: 25.8px;
-    color: #14c03e;
-    text-decoration: none; /* Remove underlines on hover */
-  }
-  
-  @media (max-width: 768px)
-  {
-    #GateStatus[data-v-dc347e84] {
-      font-size: 30px;
-      font-weight: bold;
-      text-align: center;
-      font-family: 'New Time Nevran', sans-serif;
-  }
-  }
-  
-  .table-bordered {
-      border-width: 0 1px;
-      text-align: center;
+    .faqsection {
+      margin-left: 216px;
+      padding-bottom: 30px;
   }
   
   </style>
