cbb27fd4d583b920fba764b99dc9766b60503152
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-11 14:57:08 +0530

    Update adminhome.vue

--- src/views/adminhome.vue
+++ src/views/adminhome.vue
@@ -1,8 +1,6 @@
 <script setup>
 import heder3 from '@/components/heder3.vue'
 import adminhome11 from '@/components/adminhome11.vue'
-import adminhome12 from '@/components/adminhome12.vue'
-
 </script>
 
 <template>
