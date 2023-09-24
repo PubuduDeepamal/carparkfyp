9a14f6b69c0936295a7be5f2eba6c00164022dba
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-25 05:29:14 +0530

    Update adminDashboard.vue

--- src/views/adminDashboard.vue
+++ src/views/adminDashboard.vue
@@ -1,9 +1,11 @@
 <script setup>
+import heder6 from '@/components/heder6.vue'
 import heder3 from '@/components/heder3.vue'
 </script>
 
 <template>
   <main>
+    <heder6/>
     <heder3/>
     <adminhome11/>
   </main>
