12d7041c20e9268fd783fbe2be7fbe9e025eeef3
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-25 05:57:24 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
 </script>
 
 <template> 
