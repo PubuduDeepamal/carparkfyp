65c179c9bafb61d4f8312dc07670c821930890c2
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-25 17:31:58 +0530

    Update loging.vue

--- src/components/loging.vue
+++ src/components/loging.vue
@@ -1,85 +1,104 @@
 <template>
-    <div class="login-container">
-      <div class="login-box">
-        <div class="login-header">
-          <h1 id="logingtitle" style="font-family: 'New Time Nevran', sans-serif;">Sing In</h1>
-        </div>
-        <div class="login-body">
-          <form @submit.prevent="handleSignIn">
-            <div class="form-group">
-              <input type="text" id="username" v-model="username" style="font-family: 'New Time Nevran', sans-serif;" placeholder="Username"/>
-            </div>
-            <div class="form-group">
-              <input type="password" id="password" v-model="password" placeholder="Password" style="font-family: 'New Time Nevran', sans-serif; " />
-            </div>
-            <button type="submit" class="login-button btn btn-primary" id="btnsub" style="margin-top: 20px; border: 1px solid #f0d8b6; width: 100%; font-family: 'New Time Nevran', sans-serif;" >Login</button>
-          </form>
-        </div>
-        <div id="GooleSignIn" v-if="!isSignedIn">
-          <p>
-            <button @click="handleSignInGoole" class="google-signin btn btn-primary" id="btnsub" style="margin-top: 20px; margin-bottom: 20px; border: 1px solid #f0d8b6; width: 100%; font-family: 'New Time Nevran', sans-serif;">Google Sign in</button>
-          </p>
-        </div>
+  <div class="login-container">
+    <div class="login-box">
+      <div class="login-header">
+        <h1 id="logingtitle" style="font-family: 'New Time Nevran', sans-serif;">Sign In</h1>
+      </div>
+      <div class="login-body">
+        <form @submit.prevent="handleSignIn">
+          <div class="form-group">
+            <input type="email" id="email" v-model="email" style="font-family: 'New Time Nevran', sans-serif;" placeholder="Email" />
+          </div>
+          <div class="form-group">
+            <input type="password" id="password" v-model="password" placeholder="Password" style="font-family: 'New Time Nevran', sans-serif;" />
+          </div>
+          <button type="submit" class="login-button btn btn-primary" id="btnsub" style="margin-top: 20px; border: 1px solid #f0d8b6; width: 100%; font-family: 'New Time Nevran', sans-serif;">Login</button>
+        </form>
+      </div>
+      <div id="GoogleSignIn" v-if="!isSignedIn">
+        <p>
+          <button @click="handleSignInGoogle" class="google-signin btn btn-primary" id="btnsub" style="margin-top: 20px; margin-bottom: 20px; border: 1px solid #f0d8b6; width: 100%; font-family: 'New Time Nevran', sans-serif;">Google Sign in</button>
+        </p>
+      </div>
+      <div v-if="errorMessage" class="error-message">
+        {{ errorMessage }}
       </div>
     </div>
-  </template>
-  
- 
-  
+  </div>
+</template>
+
 <script>
 import firebaseConfig from '../firebaseConfig';
-import { getAuth, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
-
-firebaseConfig
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
-    export default {
-        name: 'loging',
-        data()
-        {
-            return{
-                user:'bpunu',
-                isSignedIn: false,
-            }
-        },
-        methods:
-        {
-            handleSignInGoole(){
+firebaseConfig; // Ensure your Firebase configuration is properly set up
 
-                signInWithPopup(auth, provider)
-                    .then((result) => {
-                    this.isSignedIn =true;   
-                    this.$router.push('/Booking');    
-                    }).catch((error) => {
-                       console.log(error);
-                    });
+export default {
+  name: 'login',
+  data() {
+    return {
+      email: '',
+      password: '',
+      isSignedIn: false,
+      errorMessage: '', // Added to store error messages
+    };
+  },
+  methods: {
+    handleSignIn() {
+      const email = this.email;
+      const password = this.password;
 
-            },
-            handleSignOut(){
-                signOut(auth).then(() => {
-                   this.user = '';
-                   this.isSignedIn= false;
-                }).catch((error) => {
-                    console.log(error);
-                });
-            }
-        } 
-    }
-    
+      // Sign in with email and password
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // User successfully signed in
+          this.isSignedIn = true;
+          this.$router.push('/Booking');
+        })
+        .catch((error) => {
+          this.errorMessage = error.message; // Store the error message
+          console.error(error);
+        });
+    },
+    handleSignInGoogle() {
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          this.isSignedIn = true;
+          this.$router.push('/Booking');
+        })
+        .catch((error) => {
+          this.errorMessage = error.message; // Store the error message
+          console.error(error);
+        });
+    },
+    handleSignOut() {
+      signOut(auth)
+        .then(() => {
+          this.email = '';
+          this.password = '';
+          this.isSignedIn = false;
+          this.$router.push('/Booking');
+        })
+        .catch((error) => {
+          this.errorMessage = error.message; // Store the error message
+          console.error(error);
+        });
+    },
+  },
+};
 </script>
 
 <style src="../assets/style/loging.css"></style>
 <style src="../assets/scss/loging.scss"></style>
 
 <style scoped>
-@media (max-width:850px) {
-    .login-container .login-box 
-    {
-         width: 340px;
-    }
-    
+@media (max-width: 850px) {
+  .login-container .login-box {
+    width: 340px;
+  }
 }
 
 p {
@@ -88,32 +107,31 @@ p {
 }
 
 .login-container .login-box {
-    margin-top: 129px;
-    margin-bottom: 63px;
+  margin-top: 129px;
+  margin-bottom: 63px;
 }
 
-#logingtitle{
-    font-size: 45.8px;
-    font-weight: bold; 
+#logingtitle {
+  font-size: 45.8px;
+  font-weight: bold;
 }
 
-#btnsub{
+#btnsub {
   font-family: 'New Time Nevran', sans-serif;
   background: #14c03e;
   color: white;
 }
 
-#btnsub:hover{
+#btnsub:hover {
   background: #0086c8;
   color: white;
 }
 
-
-#fontfamily{
+#fontfamily {
   font-family: 'New Time Nevran', sans-serif;
 }
 
-.form-group{
+.form-group {
   font-family: 'New Time Nevran', sans-serif;
   border: 1px solid #14c03e;
   border-radius: 7px;
