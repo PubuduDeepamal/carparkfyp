71810cead45ceb3a044d3a5fcbb9e028248852c3
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-25 05:57:30 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,5 +14,14 @@ app.use(router)
 app.mount('#app')
 import 'bootstrap/dist/js/bootstrap.js'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faClock, faEnvelope, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHome, faClock, faEnvelope, faUserCircle)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 
