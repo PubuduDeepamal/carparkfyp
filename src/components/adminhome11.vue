407aaa89e802e12636c17634702400da412667a2
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-27 04:57:28 +0530

    Update adminhome11.vue

--- src/components/adminhome11.vue
+++ src/components/adminhome11.vue
@@ -3,7 +3,7 @@
     <br><br><br>
         <div class="container">
       <div class="section-title">
-        <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 60px;">Contact Us</h3>
+        <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 60px;">Contact Us Analysis</h3>
       </div>
       <div class="row">
         <div class="col">
