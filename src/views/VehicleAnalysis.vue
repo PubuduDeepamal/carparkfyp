0418c2fdd1142b638c144dec673384c12289079c
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-27 04:32:25 +0530

    Create VehicleAnalysis.vue

---
+++ src/views/VehicleAnalysis.vue
@@ -0,0 +1,13 @@
+<script setup>
+import heder6 from '@/components/heder6.vue'
+import heder3 from '@/components/heder3.vue'
+import Vehicleanalysis from '@/components/Vehicleanalysis.vue'
+</script>
+
+<template>
+  <main>
+    <heder3/>
+    <heder6/>
+    <Vehicleanalysis/>
+  </main>
+</template>
