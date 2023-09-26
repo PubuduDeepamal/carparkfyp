fa069b235ead69fbdcffa10847810410ae850061
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-26 15:18:25 +0530

    Update adminDashboard.vue

--- src/views/adminDashboard.vue
+++ src/views/adminDashboard.vue
@@ -2,6 +2,7 @@
 import heder6 from '@/components/heder6.vue'
 import heder3 from '@/components/heder3.vue'
 import bookingcount from '@/components/bookingcount.vue'
+import bookingcount2 from '@/components/bookingcount2.vue'
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import bookingcount from '@/components/bookingcount.vue'
     <heder6/>
     <heder3/>
     <bookingcount/>
+    <bookingcount2/>
   </main>
 </template>
