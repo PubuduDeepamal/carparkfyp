c3cd5d494932a5785f8bb383060f693057eda57e
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-26 16:05:43 +0530

    Update bookingcount3.vue

--- src/components/bookingcount3.vue
+++ src/components/bookingcount3.vue
@@ -3,12 +3,10 @@
       <br>
       <div class="container">
         <div class="section-title">
-          <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 60px;">Contact Analysis</h3>
+          <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold;">Contact Analysis</h3>
         </div>
         <div class="row">
-          <div class="col">
-            
-          </div>
+          <div class="col"></div>
           <!-- Add a chart element for daily contact count -->
           <div class="col">
             <div class="card">
@@ -31,7 +29,8 @@
     data() {
       return {
         items: [],
-        dailyContactCount: [],
+        dailyContactData: [], // Array to store daily contact data
+        chart: null, // Variable to store the chart instance
       };
     },
     mounted() {
@@ -39,15 +38,14 @@
     },
     methods: {
       fetchData() {
-        // Fetch data for both the table and the chart
+        // Fetch data for the chart
         getDocs(collection(db, 'users'))
           .then(querySnapshot => {
             querySnapshot.forEach(doc => {
               this.items.push(doc.data());
             });
-            // Call functions to render the chart and calculate the daily contact count
-            this.renderDailyContactChart();
-            this.calculateDailyContactCount();
+            // Call a function to prepare the data and render the chart
+            this.prepareDailyContactData();
           })
           .catch(error => {
             console.error('Error fetching data:', error);
@@ -56,40 +54,66 @@
       downloadCsv() {
         // ... (your CSV download code) ...
       },
-      calculateDailyContactCount() {
-        // Calculate daily contact count
-        const dailyContactCount = {};
+      prepareDailyContactData() {
+        // Calculate daily contact data
+        const dailyContactData = {};
         this.items.forEach(item => {
           const date = item.contact; // Assuming contact date is in a valid date format
-          if (dailyContactCount[date]) {
-            dailyContactCount[date]++;
+          if (dailyContactData[date]) {
+            dailyContactData[date]++;
           } else {
-            dailyContactCount[date] = 1;
+            dailyContactData[date] = 1;
           }
         });
   
-        // Convert daily contact count to an array
-        this.dailyContactCount = Object.values(dailyContactCount);
+        // Convert daily contact data to an array of objects
+        this.dailyContactData = Object.entries(dailyContactData).map(([date, count]) => ({
+          date,
+          count,
+        }));
+  
+        // Sort the data by date
+        this.dailyContactData.sort((a, b) => new Date(a.date) - new Date(b.date));
+  
+        // Render the chart
+        this.renderDailyContactChart();
       },
       renderDailyContactChart() {
+        if (this.chart) {
+          // If the chart instance already exists, destroy it first
+          this.chart.destroy();
+        }
+  
         // Create a bar chart for daily contact count
         const ctx = document.getElementById('dailyContactChart').getContext('2d');
-        new Chart(ctx, {
+        this.chart = new Chart(ctx, {
           type: 'bar',
           data: {
-            labels: Object.keys(this.dailyContactCount),
-            datasets: [{
-              label: 'Daily Contact Count',
-              data: Object.values(this.dailyContactCount),
-              backgroundColor: 'rgba(75, 192, 192, 0.2)', // Adjust color as needed
-              borderColor: 'rgba(75, 192, 192, 1)', // Adjust color as needed
-              borderWidth: 1,
-            }],
+            labels: this.dailyContactData.map(item => item.date),
+            datasets: [
+              {
+                label: 'Number of Form Submissions',
+                data: this.dailyContactData.map(item => item.count),
+                backgroundColor: 'rgba(75, 192, 192, 0.2)', // Adjust color as needed
+                borderColor: 'rgba(75, 192, 192, 1)', // Adjust color as needed
+                borderWidth: 1,
+              },
+            ],
           },
           options: {
             scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'Date',
+                },
+              },
               y: {
                 beginAtZero: true,
+                title: {
+                  display: true,
+                  text: 'Number of Form Submissions',
+                },
               },
             },
           },
