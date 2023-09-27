71287170b4aaec71dfd74c65097a32abf04760a4
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-27 12:15:11 +0530

    Update adminDashboard.vue

--- src/views/adminDashboard.vue
+++ src/views/adminDashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import heder6 from '@/components/heder6.vue'
 import heder3 from '@/components/heder3.vue'
+import Vehicleanalysis from '@/components/Vehicleanalysis.vue'
 import bookingcount from '@/components/bookingcount.vue'
 import bookingcount2 from '@/components/bookingcount2.vue'
 import bookingcount3 from '@/components/bookingcount3.vue'
@@ -10,6 +11,7 @@ import bookingcount3 from '@/components/bookingcount3.vue'
   <main>
     <heder6/>
     <heder3/>
+    <Vehicleanalysis/>
     <bookingcount/>
     <bookingcount2/>
     <bookingcount3/>
