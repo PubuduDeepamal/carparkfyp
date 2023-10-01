284541e86607c047b4a6c9014d6e1dbef2fb8ced
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-10-01 11:56:00 +0530

    Update adminhome12.vue

--- src/components/adminhome12.vue
+++ src/components/adminhome12.vue
@@ -34,6 +34,8 @@
               </table>
               <!-- Add a download button for CSV -->
               <button class="btn btn-primary" @click="downloadCsv">Download CSV</button>
+              <!-- Add a download button for analysis report -->
+              <button style="margin-left: 20px;" class="btn btn-primary" @click="downloadAnalysisReport">Download Analysis Report</button>
             </div>
           </div>
         </div>
@@ -90,6 +92,82 @@ export default {
       link.setAttribute('download', 'booking_data.csv');
       document.body.appendChild(link);
       link.click();
+    },
+    downloadAnalysisReport() {
+      // Perform your analysis here and generate a report
+      const analysisReport = this.generateAnalysisReport();
+
+      // Create a Blob object and trigger the download
+      const blob = new Blob([analysisReport], { type: 'text/plain' });
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.setAttribute('href', url);
+      link.setAttribute('download', 'analysis_report.txt');
+      document.body.appendChild(link);
+      link.click();
+    },
+    generateAnalysisReport() {
+      // Perform your analysis here and generate a report as a string
+      let report = 'Analysis Report:\n\n';
+
+      // Add your analysis logic here
+      const totalBookings = this.items.length;
+      report += `Total Bookings: ${totalBookings}\n`;
+
+      const averageContactLength = this.calculateAverageContactLength();
+      report += `Average Contact Number Length: ${averageContactLength.toFixed(2)}\n`;
+
+      const mostCommonDate = this.calculateMostCommonDate();
+      report += `Most Common Date: ${mostCommonDate}\n`;
+
+      const mostCommonTime = this.calculateMostCommonTime();
+      report += `Most Common Time: ${mostCommonTime}\n`;
+
+      // Add more analysis details here as needed
+
+      return report;
+    },
+    calculateAverageContactLength() {
+      // Calculate the average length of contact numbers
+      const totalLength = this.items.reduce((acc, item) => {
+        return acc + (item.contact ? item.contact.length : 0);
+      }, 0);
+
+      const averageLength = totalLength / this.items.length;
+
+      return averageLength;
+    },
+    calculateMostCommonDate() {
+      // Calculate the most common date
+      const dateCountMap = {};
+      this.items.forEach(item => {
+        const date = item.dateInput;
+        if (date in dateCountMap) {
+          dateCountMap[date]++;
+        } else {
+          dateCountMap[date] = 1;
+        }
+      });
+
+      const mostCommonDate = Object.keys(dateCountMap).reduce((a, b) => dateCountMap[a] > dateCountMap[b] ? a : b);
+
+      return mostCommonDate;
+    },
+    calculateMostCommonTime() {
+      // Calculate the most common time
+      const timeCountMap = {};
+      this.items.forEach(item => {
+        const time = item.timeInput;
+        if (time in timeCountMap) {
+          timeCountMap[time]++;
+        } else {
+          timeCountMap[time] = 1;
+        }
+      });
+
+      const mostCommonTime = Object.keys(timeCountMap).reduce((a, b) => timeCountMap[a] > timeCountMap[b] ? a : b);
+
+      return mostCommonTime;
     },
   },
 };
