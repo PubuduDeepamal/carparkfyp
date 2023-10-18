73d8b2b19a0840248fafa9b304cd41a68e106d47
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-10-18 15:41:01 +0530

    Update Vehicleanalysis.vue

--- src/components/Vehicleanalysis.vue
+++ src/components/Vehicleanalysis.vue
@@ -1,21 +1,21 @@
 <template>
   <br><br><br>
   <div class="container mt-5">
-    <h1 class="text-center" style="font-size: 45.8px; font-weight: bold;" id="fontfamily">Parking Slot Status</h1>
+    <h1 class="text-center faqsection" style="font-size: 45.8px; font-weight: bold;" id="fontfamily">Parking Slot Status</h1>
     <div class="row justify-content-center mt-4">
       <div class="col-md-6">
         <div class="card text-center">
           <div class="card-body">
-            <h2 class="card-title">Available Slots</h2>
-            <p class="card-text display-4">{{ availableSlots }}</p>
+            <h2 class="card-title" id="fontfamily">Available Slots</h2>
+            <p class="card-text display-4" id="fontfamily">{{ availableSlots }}</p>
           </div>
         </div>
       </div>
       <div class="col-md-6">
         <div class="card text-center">
           <div class="card-body">
-            <h2 class="card-title">Unavailable Slots</h2>
-            <p class="card-text display-4">{{ unavailableSlots }}</p>
+            <h2 class="card-title" id="fontfamily">Unavailable Slots</h2>
+            <p class="card-text display-4" id="fontfamily">{{ unavailableSlots }}</p>
           </div>
         </div>
       </div>
@@ -53,5 +53,10 @@ export default {
 <style>
 #fontfamily {
   font-family: 'New Time Nevran', sans-serif;
+}
+
+.faqsection {
+    margin-left: 216px;
+    padding-bottom: 30px;
 }
 </style>
