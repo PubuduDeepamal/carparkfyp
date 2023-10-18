0de1359c34fb9f93114733f4ee632367c7666229
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-10-18 15:56:44 +0530

    Update Vehicleanalysis1.vue

--- src/components/Vehicleanalysis1.vue
+++ src/components/Vehicleanalysis1.vue
@@ -1,6 +1,6 @@
 <template>
-    <div style="background-color: #f1f7fc;">
-        <br><br><br>
+    <div>
+        <br>
         <div class="container mt-5">
             <div class="row justify-content-center mt-4">
                 <div class="col-md-6">
@@ -21,7 +21,7 @@
                 </div>
             </div>
         </div>
-        <br><br>
+        <br>
       </div>
     </template>
     
