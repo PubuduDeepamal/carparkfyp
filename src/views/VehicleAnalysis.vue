741e9f50e1dfb70d2ccd3760694b73e1bd15a04a
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-10-21 08:10:51 +0530

    Update VehicleAnalysis.vue

--- src/views/VehicleAnalysis.vue
+++ src/views/VehicleAnalysis.vue
@@ -2,6 +2,7 @@
 import heder6 from '@/components/heder6.vue'
 import heder3 from '@/components/heder3.vue'
 import Vehiclecount from '@/components/Vehiclecount.vue'
+import vcount from '@/components/vcount.vue'
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import Vehiclecount from '@/components/Vehiclecount.vue'
     <heder3/>
     <heder6/>
     <Vehiclecount/>
+    <vcount/>
   </main>
 </template>
